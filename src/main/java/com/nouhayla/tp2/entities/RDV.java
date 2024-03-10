780e3e7678bdd50bc5b9a0654fcaae6631e0830d
Nouhayla MOUAKKAL <86119525+NouhaylaMouakkal@users.noreply.github.com>
2024-03-10 18:44:19 +0000

    finish

--- src/main/java/com/nouhayla/tp2/entities/RDV.java
+++ src/main/java/com/nouhayla/tp2/entities/RDV.java
@@ -21,6 +21,6 @@ public class RDV {
     private Patient patient;
     @ManyToOne
     private Medecin medecin;
-    @OneToOne(mappedBy = "rdvs")
+    @OneToOne(mappedBy = "rdv")
     private Consultation consultation;
 }
